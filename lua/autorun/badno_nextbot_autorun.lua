5a49db4d1f516496312e70f65601a534f68f159b
MrDadel <115901035+MrDadel@users.noreply.github.com>
2026-05-31 16:36:07 -0400

    Rename jerma_nextbot_autorun.lua to badno_nextbot_autorun.lua

---
+++ lua/autorun/badno_nextbot_autorun.lua
@@ -0,0 +1,9 @@
+local doingDynamicContent = CreateConVar( "badno_dynamic_content", 0, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Dynamically request the badno sounds whenever a bot is spawned?" )
+
+if not SERVER then return end
+
+if not doingDynamicContent:GetBool() then -- this will only work after a server restart with the new convar!
+    resource.AddWorkshop( "3330585475" ) -- bot ( sounds )
+    resource.AddWorkshop( "2691974423" ) -- playermodel
+
+end
